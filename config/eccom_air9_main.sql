76e3480b1cb3f7dd3085d1f44cce2bbb4c2841c3
frontendplux <frontendplux@gmail.com>
2026-04-07 08:14:45 -0700

    v1

--- config/eccom_air9_main.sql
+++ config/eccom_air9_main.sql
@@ -328,3 +328,13 @@ CREATE TABLE IF NOT EXISTS promotion_categories (
     UNIQUE (promotion_id, category_id)
 );
 
+
+
+ALTER TABLE products
+ADD COLUMN IF NOT EXISTS brand_id INT AFTER category_id,
+ADD INDEX idx_brand_id (brand_id),
+ADD CONSTRAINT fk_product_brand
+    FOREIGN KEY (brand_id)
+    REFERENCES brands(id)
+    ON DELETE SET NULL;
+
